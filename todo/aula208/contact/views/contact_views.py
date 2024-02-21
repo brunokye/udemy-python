6231ab949b277f81bc92bee82acb0bc7f40b0b5a
brunokye <contatobrunokye@gmail.com>
2024-02-21 14:57:02 -0300

    feat: cria parciais para head e header

--- todo/aula208/contact/views/contact_views.py
+++ todo/aula208/contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
 
     context = {
         "contacts": contacts,
+        "site_title": "Contatos",
     }
 
     return render(request, "contact/index.html", context)
@@ -15,9 +16,11 @@ def index(request):
 def contact(request, contact_id):
     # single_contact = Contact.objects.get(pk=contact_id, show=True)
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact_name = f"{single_contact.first_name} {single_contact.last_name}"
 
     context = {
         "contact": single_contact,
+        "site_title": contact_name,
     }
 
     return render(request, "contact/contact.html", context)
