05ad8661d276223abde2d2af80628d44e6b6bcac
brunokye <contatobrunokye@gmail.com>
2024-02-21 14:43:00 -0300

    feat: cria página para exibir um único contato

--- todo/aula208/contact/urls.py
+++ todo/aula208/contact/urls.py
@@ -3,4 +3,7 @@ from contact import views
 
 app_name = "contact"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("<int:contact_id>", views.contact, name="contact"),
+    path("", views.index, name="index"),
+]
