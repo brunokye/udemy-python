82a0cdbfecdc07dac18aa647bf51617a17853fa8
brunokye <contatobrunokye@gmail.com>
2024-02-29 16:50:23 -0300

    feat: adiciona campos e erros ao formulário

--- todo/aula208/contact/views/contact_views.py
+++ todo/aula208/contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 
 from contact.models import Contact
