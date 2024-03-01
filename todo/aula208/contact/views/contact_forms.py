47745685673490f405786f895003aae950ec31ab
brunokye <contatobrunokye@gmail.com>
2024-03-01 16:09:00 -0300

    feat: organiza arquivos do formulário

--- todo/aula208/contact/views/contact_forms.py
+++ todo/aula208/contact/views/contact_forms.py
@@ -1,24 +1,5 @@
-from typing import Any
-from django import forms
 from django.shortcuts import render
-from django.core.exceptions import ValidationError
-
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ("first_name", "last_name", "phone", "email")
-
-    def clean(self) -> dict[str, Any]:
-        self.add_error(
-            None, ValidationError("Mensagem de erro", code="invalid")
-        )
-        self.add_error(
-            None, ValidationError("Mensagem de erro 2", code="invalid")
-        )
-        return super().clean()
+from contact.forms import ContactForm
 
 
 def create(request):
