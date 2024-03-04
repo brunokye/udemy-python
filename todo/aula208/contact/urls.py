deb9bfcbea7ea37d1a7e58a8a42c1e7221ea1188
brunokye <contatobrunokye@gmail.com>
2024-03-04 17:33:22 -0300

    feat: adiciona método update

--- todo/aula208/contact/urls.py
+++ todo/aula208/contact/urls.py
@@ -6,6 +6,7 @@ app_name = "contact"
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
-    path("contact/<int:contact_id>/detail/", views.contact, name="contact"),
     path("contact/create", views.create, name="create"),
+    path("contact/<int:contact_id>/detail/", views.contact, name="contact"),
+    path("contact/<int:contact_id>/update/", views.update, name="update"),
 ]
