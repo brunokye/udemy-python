3b96c3666ca2bfa52c2782a1d20492dd3e4da7ff
brunokye <contatobrunokye@gmail.com>
2024-04-03 16:02:35 -0300

    feat: atualiza layout e css do blog

--- secao-12-projeto-blog/djangoapp/blog/views.py
+++ secao-12-projeto-blog/djangoapp/blog/views.py
@@ -1,13 +1,26 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 
 def index(request):
-    return render(request, "blog/pages/index.html")
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
-
-def post(request):
-    return render(request, "blog/pages/post.html")
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        },
+    )
 
 
 def page(request):
     return render(request, "blog/pages/page.html")
+
+
+def post(request):
+    return render(request, "blog/pages/post.html")
