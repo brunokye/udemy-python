84e5e1020d8470074ee3a45443500fc59c2fe8f2
brunokye <contatobrunokye@gmail.com>
2024-04-04 18:06:59 -0300

    feat: adiciona model page

--- secao-12-projeto-blog/djangoapp/blog/models.py
+++ secao-12-projeto-blog/djangoapp/blog/models.py
@@ -18,6 +18,9 @@ class Tag(models.Model):
 
         return super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     class Meta:
@@ -34,3 +37,34 @@ class Category(models.Model):
             self.slug = slugify_new(self.name, 4)
 
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    class Meta:
+        verbose_name = "Page"
+        verbose_name_plural = "Pages"
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default=None, null=True, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            "Este campo precisará estar marcado "
+            "para a página ser exibida publicamente."
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
