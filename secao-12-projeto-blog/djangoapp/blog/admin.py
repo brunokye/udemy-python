99d01660e1a041599166d52abf75236f9436e544
brunokye <contatobrunokye@gmail.com>
2024-04-05 16:15:03 -0300

    feat: adiciona summernote como editor html das pages e posts

--- secao-12-projeto-blog/djangoapp/blog/admin.py
+++ secao-12-projeto-blog/djangoapp/blog/admin.py
@@ -1,6 +1,6 @@
-from typing import Any
 from django.contrib import admin
 from blog.models import Tag, Category, Page, Post
+from django_summernote.admin import SummernoteModelAdmin  # type: ignore
 
 
 @admin.register(Tag)
@@ -44,7 +44,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
     list_display = (
         "id",
         "title",
@@ -64,7 +65,8 @@ class PageAdmin(admin.ModelAdmin):
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
     list_display = (
         "id",
         "title",
