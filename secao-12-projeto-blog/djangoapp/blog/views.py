caef0a10840e69607ea3018ae0a03fe5bd306797
brunokye <contatobrunokye@gmail.com>
2024-04-09 17:36:04 -0300

    feat: adiciona funcionalidade ao botão de search

--- secao-12-projeto-blog/djangoapp/blog/views.py
+++ secao-12-projeto-blog/djangoapp/blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.db.models import Q
 from blog.models import Post
+
 
 PER_PAGE = 9
 
@@ -77,5 +79,24 @@ def tag(request, slug):
         "blog/pages/index.html",
         {
             "page_obj": page_obj,
+        },
+    )
+
+
+def search(request):
+    search_value = request.GET.get("search", "").strip()
+
+    posts = Post.objects.get_published().filter(  # type: ignore
+        Q(title__icontains=search_value)
+        | Q(excerpt__icontains=search_value)
+        | Q(content__icontains=search_value)
+    )[:PER_PAGE]
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": posts,
+            "search_value": search_value,
         },
     )
