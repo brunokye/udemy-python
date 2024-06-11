b63fca27436803e29f25c1c2bf5906233276731c
brunokye <contatobrunokye@gmail.com>
2024-06-11 14:22:47 -0300

    feat: greedy e lazy

--- secao-18-regular-expressions/aula004.py
+++ secao-18-regular-expressions/aula004.py
@@ -8,4 +8,4 @@ import re
 text = "<p>Frase 1</p> <p>:)</p> <p>Test</p> <div></div>"
 
 print(re.findall(r"<[pdiv]{1,3}>.*<\/[pdiv]{1,3}>", text))  # greedy
-print(re.findall(r"<[pdiv]{1,3}>.*?<\/[pdiv]{1,3}>", text))  # non greedy
+print(re.findall(r"<[pdiv]{1,3}>.*?<\/[pdiv]{1,3}>", text))  # lazy
