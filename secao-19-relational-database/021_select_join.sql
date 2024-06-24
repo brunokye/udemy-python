1b1e62b94c1bc87e66628ea8f2156202385707c1
brunokye <contatobrunokye@gmail.com>
2024-06-24 18:54:43 -0300

    feat: select join

---
+++ secao-19-relational-database/021_select_join.sql
@@ -0,0 +1,5 @@
+SELECT u.id AS uid, u.first_name, p.bio, r.name FROM users AS u
+LEFT JOIN profiles AS p ON u.id = p.user_id
+INNER JOIN users_roles AS ur ON u.id = ur.user_id
+INNER JOIN roles AS r ON ur.role_id = r.id
+ORDER BY uid ASC;
